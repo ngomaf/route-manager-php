572bf0b28657f09eb17e42b277348d80772ad9eb
Ngoma Fortuna <ngomafortuna@gmail.com>
2024-07-05 21:13:36 +0100

    Refatoration ManagerRoute.
    Close #3

--- routes/ManagerRoute.php
+++ routes/ManagerRoute.php
@@ -15,7 +15,7 @@ class ManagerRoute {
     private $tam;
     private $ctrl;
     private $dir;
-    private $page;
+    private $transl;
     private $verbs = [];
     private $path = DIR .'src/controllers/';
 
@@ -24,52 +24,56 @@ class ManagerRoute {
         $this->tam=count($this->url); 
 
         // url translator
-        $this->page=TranslateToPt::get();
+        $this->transl=TranslateToPt::get();
 
         $this->verifyParam();
     }
 
     private function verifyParam() {
-        $item = isset($this->page[$this->url[0]])? $this->page[$this->url[0]]: $this->url[0];
+        $item = isset($this->transl[$this->url[0]])? $this->transl[$this->url[0]]: $this->url[0];
         if($this->tam == 1 && empty($item)) {
             // empty url condition
-            $this->ctrl = new \Controllers\site\Home;
-            $this->ctrl->index();
-
+            $object = new \Controllers\site\Home;
+            $object->index();
             return false;
         } elseif ($this->tam==1 && !empty($item)) {
             // if url heve one element
             $ctrl = ucfirst($item);
             if(is_dir($this->path.$item)) {
                 // if url element exist end is a directory
-                $ClassName = "Controllers\\{$item}\\Start";
-                $object = new $ClassName();
+                $Class = "Controllers\\{$item}\\Start";
+                $object = new $Class();
                 $object->index();
             } elseif(file_exists($this->path.'site/'.$ctrl.'.php')) {
                 // if url element exist in path controllers/site
-                $ClassName = "Controllers\\site\\{$ctrl}";
-                $object = new $ClassName();
+                $Class = "Controllers\\site\\{$ctrl}";
+                $object = new $Class();
                 $object->index();
             } else {
-                $this->ctrl = new \Controllers\site\Notice;
-                $this->ctrl->show($this->url[0]);
+                // main service or page, for examplo Post in one blog
+                // Only need edit Notice
+                $object = new \Controllers\site\Notice;
+                if(method_exists($object, 'show')) 
+                {$object->show($this->url[0]);}
+                else {new ErrorMsg();}
+                return false;
             }
-            
             return false;
         } elseif($this->tam<4 && !is_dir($this->path.$item)) {
-            // condetion to permit remove show verb in url
+            // condition to permit remove show verb in url
             $ctrl = ucfirst($item);
-            $ClassName = "Controllers\\site\\{$ctrl}";
-            $object = new $ClassName();
+            if(file_exists($this->path.'site/'.$ctrl.'.php'))
+            {$Class = "Controllers\\site\\{$ctrl}"; $object = new $Class();}
+            else {new ErrorMsg(); return false;}
 
-            $param = isset($this->page[$this->url[1]])? $this->page[$this->url[1]]: $this->url[1];
+            $param = isset($this->transl[$this->url[1]])? $this->transl[$this->url[1]]: $this->url[1];
             if(method_exists($object, $param)) {
                 if($param=='show') {$object->show($this->url[2]);}
                 else 
                 {
-                    $url[]=isset($this->page[$this->url[1]])? $this->page[$this->url[1]]: $this->url[1];
+                    $url[]=isset($this->transl[$this->url[1]])? $this->transl[$this->url[1]]: $this->url[1];
                     if(isset($this->url[2])) $url[]=$this->url[2];
-                    (isset($url[1]) && isset($this->page[$url[1]]))? $url[1]=$this->page[$url[1]]: null;
+                    (isset($url[1]) && isset($this->transl[$url[1]]))? $url[1]=$this->transl[$url[1]]: null;
                     $object->{$param}($url);
                 }
                 return false;
@@ -80,41 +84,31 @@ class ManagerRoute {
                 return false;
             }
             $object->index();
-
         } else {
             $this->verifyDir();
         }
     }
 
     private function verifyDir() {        
-
         for($i=0; $i<$this->tam; $i++){
-            $item = isset($this->page[$this->url[$i]])? $this->page[$this->url[$i]]: $this->url[$i];
+            $item = isset($this->transl[$this->url[$i]])? $this->transl[$this->url[$i]]: $this->url[$i];
 
             if(is_dir($this->path.$item)) {
-
                 $this->path .= $item.'/';
                 $this->dir = $item;
-                
             } else {
-                
                 $ctrl = ucfirst($item);                
                 if(file_exists($this->path . $ctrl .'.php')){
                     $this->ctrl = $ctrl;
                 } else {
                     array_push($this->verbs, $item);
                 }
-
             }
         }
+        if($this->dir==null) {new ErrorMsg(); return false;}
 
-        if($this->dir==null) {
-            new ErrorMsg();
-            return false;
-        }
-
-        $ClassName = "Controllers\\{$this->dir}\\{$this->ctrl}";
-        $object = new $ClassName();
+        $Class = "Controllers\\{$this->dir}\\{$this->ctrl}";
+        $object = new $Class();
         
         if(sizeof($this->verbs)==0) {
             $object->index();
@@ -135,7 +129,7 @@ class ManagerRoute {
             {$object->{$this->verbs[0]}($this->verbs);}
             else {new ErrorMsg();}
         }
-
     }
-    
 }
+
+// 148 lines
